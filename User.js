abd5fd7604a0d0f53eef48ab30f4e4f877ddc1dd
Louie Reyes <louiereyes0424@gmail.com>
2025-02-22 02:33:25 +0800

    Delete User.js
    cleanup

--- User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    role: { type: String, enum: ['user', 'admin'], required: true }
-});
-
-module.exports = mongoose.model('User', UserSchema);
